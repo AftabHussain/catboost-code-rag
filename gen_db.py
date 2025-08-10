14995b6a0dee2b70c7ef64b617779a85b596c7d2
Aftab Hussain <aftab.hussain46@gmail.com>
2025-08-10 08:47:46 +0000

    update vectorstore generation to include new synthetic dataset, and include the ragvectordb

--- gen_db.py
+++ gen_db.py
@@ -3,12 +3,19 @@ from langchain.schema import Document
 from langchain_huggingface import HuggingFaceEmbeddings
 import pandas as pd
 
-# Load CSV data
+# LOAD CSV DATA
+'''
 df = pd.read_csv('samples/zillow_catboost_samples_16_to_25.csv')
 df2 = pd.read_csv('samples/zillow_catboost_samples_26_to_100.csv')
 df3 = pd.read_csv('samples/zillow_catboost_samples_101_to_103.csv')
 df = pd.concat([df, df2], ignore_index=True)
 df = pd.concat([df, df3], ignore_index=True)
+'''
+
+# Latest dataset (synthetically generated, template-driven dataset of diverse
+# CatBoost code snippets and descriptions for housing data modeling) 
+df = pd.read_csv('samples/catboost_code_dataset_from_templates.csv')
+
 
 
 # Prepare documents list by combining code and description columns
@@ -24,5 +31,5 @@ embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-Mi
 vectorstore = FAISS.from_documents(documents, embedding_model)
 
 # Save the vectorstore locally
-vectorstore.save_local("rag_vectorstore_db_v3")
+vectorstore.save_local("rag_vectorstore_db_v4")
 
